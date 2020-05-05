1cf95bf47c90557ffa06e1d496e801a7160fd5c4
Harry <harinder.sharma@koovs.com>
2020-05-05 11:17:54 +0530

    feat(singly linked list)

---
+++ linkedlist.js
@@ -0,0 +1,83 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    const lastNode = this.tail;
+    lastNode.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  _traverseToIndex(index) {
+    let i = 0;
+    let currentNode = this.head;
+    while (i !== index) {
+      currentNode = currentNode.next;
+      i++;
+    }
+    return currentNode;
+  }
+
+  insert(index, value) {
+    const newNode = new Node(value);
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const leader = this._traverseToIndex(index - 1);
+    newNode.next = leader.next;
+    leader.next = newNode;
+    this.length;
+    return this.printList();
+  }
+
+  remove(index) {
+    const leader = this._traverseToIndex(index - 1);
+    const toBeRemoved = leader.next;
+    leader.next = toBeRemoved.next;
+    this.length--;
+    return this.printList();
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 88);
+myLinkedList.remove(5);
+console.log(myLinkedList.printList());
